ff18b0858eb6d5138bff33dbeb7173904f57b9f9
Blaiva <blayverthreyesp14@gmail.com>
2026-06-27 23:34:05 -0400

    Modifica el MainActivity.kt

--- app/src/main/java/edu/ucne/apiplanetsblayverth/presentation/navigation/AppNavHost.kt
+++ app/src/main/java/edu/ucne/apiplanetsblayverth/presentation/navigation/AppNavHost.kt
@@ -1,9 +1,13 @@
 package edu.ucne.apiplanetsblayverth.presentation.navigation
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import edu.ucne.apiplanetsblayverth.presentation.detail.character.CharacterDetailScreen
 import edu.ucne.apiplanetsblayverth.presentation.detail.planet.PlanetDetailScreen
 import edu.ucne.apiplanetsblayverth.presentation.list.character.CharacterListScreen
@@ -11,11 +15,13 @@ import edu.ucne.apiplanetsblayverth.presentation.list.planet.PlanetListScreen
 
 @Composable
 fun AppNavHost(
-    navHostController: NavHostController
+    navHostController: NavHostController = rememberNavController(),
+    innerPadding: PaddingValues
 ){
     NavHost(
         navController = navHostController,
-        startDestination = Screen.PlanetList
+        startDestination = Screen.PlanetList,
+        modifier = Modifier.padding(innerPadding)
     ){
         composable<Screen.PlanetList> {
             PlanetListScreen(
